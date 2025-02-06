91d8905064626384296da3b617ef1573eda1ba00
Gyovanna Rayani Siqueira da Silva <20221074010034@ifrn.local>
2025-02-06 15:18:50 -0300

    Atividade Desafio

--- src/ui3inf2v/TriangulosUI.java
+++ src/ui3inf2v/TriangulosUI.java
@@ -4,6 +4,8 @@
  */
 package ui3inf2v;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 20221074010034
@@ -50,6 +52,11 @@ public class TriangulosUI extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Triângulos");
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
 
         jSeparator1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -173,9 +180,23 @@ public class TriangulosUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+        
+    public atatic boolean isTriangle(double a, double b, double c){
+            return a < b + c && c < a + b;
+    }
+    public static String typeTriangle (double a, double b, double c)
+            if (a == b )
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-                    
+              JOptionPane.showMessageDialog(
+                    null,
+                      "preencha todos os lados!",
+                      "ERRO",
+                      JOptionPane.ERROR_MESSAGE
+                      
+);
+                     
+        
+       
         double a = Double.parseDouble(A.getText());
         double b = Double.parseDouble(B.getText());
         double c = Double.parseDouble(C.getText());
@@ -195,36 +216,11 @@ public class TriangulosUI extends javax.swing.JFrame {
         } else areaLabel.setText("Não é um triangulo");                                    
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TriangulosUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TriangulosUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TriangulosUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TriangulosUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jLabel1MouseClicked
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+    /*
                 new TriangulosUI().setVisible(true);
             }
         });
@@ -245,4 +241,3 @@ public class TriangulosUI extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JLabel tipoLabel;
     // End of variables declaration//GEN-END:variables
-}
